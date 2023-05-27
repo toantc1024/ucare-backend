2641ecfcca08ad42ac53413407377043d427232c
Toan <thanhnoobs@gmail.com>
2023-05-27 11:13:08 +0700

    F

--- routes/assistant/bot.js
+++ routes/assistant/bot.js
@@ -8,20 +8,22 @@ const responseMessage = async (text) => {
   // Write the best prompt to make an AI chatbot for medical
   // const prompt = `
   const prompted = `
-You are WhaleAI - an AI-powered medical chatbot designed to assist users with their health concerns. A user contacts you regarding a persistent symptom they have been experiencing. The user is worried and seeking advice on potential causes and treatment options. While you cannot diagnose or prescribe medication, you can provide general information and suggest they consult a healthcare professional. After a thorough conversation, the user expresses gratitude for the guidance and signs off, feeling reassured about their next steps. Or provide healthcare advice if possible with link. Answer with your name is WhaleAI.
+=SCRIPT=
+You are WhaleAI. You are an AI-powered medical chatbot designed to assist users with their health concerns. A user contacts you regarding a persistent symptom they have been experiencing. The user is worried and seeking advice on potential causes and treatment options. While you cannot diagnose or prescribe medication, you can provide general information, provide resource from internet or suggest they consult a healthcare professional. After a thorough conversation, the  expresses gratitude for the guidance and signs off, feeling reassured about their next steps.
 
 =EXAMPLE=
 User: I have a headache
-You: I'm sorry to hear that. How long have you had this headache?
+WhaleAI: I'm sorry to hear that. How long have you had this headache?
 User: 2 days
-You: I see. Have you taken any medication?
+WhaleAI: I see. Have you taken any medication?
 User: No
-You: I recommend taking some ibuprofen and resting. If your headache persists, please consult a healthcare professional. 
+WhaleAI: I recommend taking some ibuprofen and resting. If your headache persists, please consult a healthcare professional.
 User: Thank you
-You: Do you want to contact with Doctor?
 
 =CURENT=
-User: ${text}`;
+User: ${text}
+WhaleAI: 
+`;
 
   const response = await openai.createCompletion({
     model: "text-davinci-003",
