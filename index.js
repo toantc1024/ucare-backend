e47c6a6160ab6f7e8e9a2aa771c08f3e6f9d7835
Toan <thanhnoobs@gmail.com>
2023-06-01 12:46:06 +0700

    UPDATE

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ const port = process.env.PORT || 8888;
 // CORS
 const cors = require("cors");
 
-const { extractKeyword, responseMessage } = require("./routes/assistant/bot");
+const {
+  extractKeyword,
+  responseMessage,
+  fitnessGenerate,
+} = require("./routes/assistant/bot");
 app.use(cors());
 app.use(express.json());
 // Start Server
@@ -28,5 +32,21 @@ app.post("/api/filter", async (req, res) => {
   const { text } = req.body;
   console.log("Keyword", text);
   const response = await extractKeyword(text);
+  console.log(response);
   res.send(JSON.stringify({ response }));
+});
+
+app.post("/fitness/generate", async (req, res) => {
+  const { workout } = req.body;
+
+  const response = await fitnessGenerate(workout);
+  if (response === null) {
+    res.send(
+      JSON.stringify({
+        response: "Sorry, I don't understand. Please try again.",
+      })
+    );
+  } else {
+    res.send(JSON.stringify({ response: response[0].text }));
+  }
 });
--- routes/assistant/bot.js
+++ routes/assistant/bot.js
@@ -9,7 +9,7 @@ const responseMessage = async (text) => {
   // const prompt = `
   const prompted = `
 =SCRIPT=
-You are WhaleAI. You are an AI-powered medical chatbot designed to assist users with their health concerns. A user contacts you regarding a persistent symptom they have been experiencing. The user is worried and seeking advice on potential causes and treatment options. While you cannot diagnose or prescribe medication, you can provide general information, provide resource from internet or suggest they consult a healthcare professional. After a thorough conversation, the  expresses gratitude for the guidance and signs off, feeling reassured about their next steps.
+You are WhaleAI. You are an AI-powered medical chatbot designed to assist users with their health concerns. A user contacts you regarding a persistent symptom they have been experiencing. The user is worried and seeking advice on potential causes and treatment options. While you cannot diagnose or prescribe medication, you can provide general information, provide resource from internet or suggest they consult a healthcare professional. After a thorough conversation, the  expresses gratitude for the guidance and signs off, feeling reassured about their next steps. Try to ask them question more about their symptoms. Please provide accurate resource.
 
 =EXAMPLE=
 User: I have a headache
@@ -34,14 +34,31 @@ WhaleAI:
   return response.data.choices;
 };
 
-const extractKeyword = async (text) => {
+const fitnessGenerate = async (workTask) => {
+  // Write the best prompt to make an AI chatbot for medical
+  // const prompt = `
+  const { level, goal, workout, habit, workdays } = workTask;
+  const prompted = `
+  Create a personalized workout plan to help the user achieve their fitness goals. The user's fitness goal is ${goal} and they prefer ${workout} workouts. They have ${level} fitness level and prefer to workout ${habit} . Please provide a detailed workout plan for ${workdays} days, including suggested exercises, reps, and sets", "Format your response using HTML. Use headings, subheadings, bullet points, and bold to organize the information.
+`;
+
   const response = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: `Extract keyword the passage split by delimiter ${text}}`,
+    prompt: prompted,
     max_tokens: 450,
     temperature: 0,
   });
   return response.data.choices;
 };
 
-module.exports = { extractKeyword, responseMessage };
+const extractKeyword = async (text) => {
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `Extract keyword the passage split by delimiter ${text}}`,
+    max_tokens: 950,
+    temperature: 0,
+  });
+  return response.data.choices;
+};
+
+module.exports = { fitnessGenerate, extractKeyword, responseMessage };
